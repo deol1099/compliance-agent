e2cd8c06c492e430ec1ce55ca6b5d07d7fecb147
deol1099 <113921776+deol1099@users.noreply.github.com>
2025-05-02 22:42:07 -0600

    Add files via upload

---
+++ Pdf-Merger/src/main/java/com/complianceagent/Pdf/Merger/PdfMergerApplication.java
@@ -0,0 +1,13 @@
+package com.complianceagent.Pdf.Merger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfMergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfMergerApplication.class, args);
+	}
+
+}
---
+++ Pdf-Merger/src/test/java/com/complianceagent/Pdf/Merger/PdfMergerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complianceagent.Pdf.Merger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfMergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
