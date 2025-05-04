1faff3e5e33b9447c9f02cc02177d9b15d2c911c
shubhkaran30546 <73439286+shubhkaran30546@users.noreply.github.com>
2025-05-03 23:34:09 -0600

    working on UI

--- compliance-app/src/components/Features.js
+++ compliance-app/src/components/Features.js
@@ -1,12 +1,40 @@
+import "./Features.css"
 const Features = () => (
-    <section className="features">
-        <h2>Why Compliance Agent?</h2>
-        <ul>
-            <li>Automated Merging</li>
-            <li>Dropbox Integration</li>
-            <li>Faster Compliance</li>
-            <li>User-Friendly Interface</li>
-        </ul>
-    </section>
+    <div className="features1">
+        <img className="features-img" src="pic2.png" alt="Feature 1" />
+        <div className="features">
+            <h2>Why Compliance Agent?</h2>
+            <ul className="features-list">
+                <li>
+                    <img src="automated merging.png" alt="Automated Merging Icon"/>
+                    <div>
+                        <strong>Automated Merging</strong>
+                        <p>Say goodbye to manual PDF work.</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="dropbox.png" alt="Dropbox-Integration Icon"/>
+                    <div>
+                        <strong>Dropbox Integration</strong>
+                        <p>Secure and seamless file handling.</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="faster-compliance.png" alt="Faster Compliance Icon"/>
+                    <div>
+                        <strong>Faster Compliance</strong>
+                        <p>Complete compliance packaging in seconds.</p>
+                    </div>
+                </li>
+                <li>
+                    <img src="user-friendly.png" alt="User Friendly Interface Icon"/>
+                    <div>
+                        <strong>User-Friendly Interface</strong>
+                        <p>Built for non-tech-savvy team members.</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 );
 export default Features;
